105bb70f56b8ccaccd1f6926395bdeb47b53f27e
phuphaGod <105551327+phuphaGod@users.noreply.github.com>
2023-07-17 10:12:09 +0700

    Add files via upload

---
+++ Exercise 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{  
+	cout << "********Exercise 2********" << endl;
+	string Name;
+	int Year_of_birth;
+
+	cout << "Enter Name : ";
+	cin >> Name;
+	cout << "Enter Year of birth : ";
+	cin >> Year_of_birth;
+	cout << "********Outout********" << endl;
+	cout << "Your Age = : " <<2566-Year_of_birth <<"year old " << endl;	
+	return (0);
+}
